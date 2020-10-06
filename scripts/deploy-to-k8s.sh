c7e45ceb90f58ca7cfccfe405e0f6c79210d7729
rajakalyanram.s@gmail.com <rajakalyanram.s@gmail.com>
2020-10-06 11:10:05 +0100

    deploy k8s updated

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -3,8 +3,8 @@ set -ex
 
 kubectl apply -f deployment/pages-namespace.yaml
 
-kubectl delete deploy pages
-kubectl delete svc pages
+kubectl delete deploy pages -nbeta
+kubectl delete svc pages -nbeta
 
 kubectl apply -f deployment/pages-config.yaml
 kubectl apply -f deployment/pages-service.yaml
